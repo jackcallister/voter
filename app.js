be5ea2f34a615538866a5eb6660ad713602bee89
Jack Callister <jarsbe@gmail.com>
2013-09-21 20:48:12 +1200

    Use less, awesome.

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var http    = require('http');
+var less    = require('less-middleware');
 var routes  = require('./routes');
 
-var app     = express();
+var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(app.router);
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+
+  app.use(less({ src: __dirname + '/public', force: true }));
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
 
 app.get('/', routes.index);
 
